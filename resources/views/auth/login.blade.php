918d53f0cd0e02fde8c426e67b6287858258fef5
andito28 <andito763@gmail.com>
2021-10-16 20:09:51 +0800

    install laravel socialite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-4">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -62,6 +62,12 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-2">
+                            <div class="col-md-8 offset-md-4">
+                                <a href="" class="btn btn-primary">Login with google</a>
                             </div>
                         </div>
                     </form>
