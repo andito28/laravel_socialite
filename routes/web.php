247343a1fc8ae6b2f59844343d715c0296331737
andito28 <andito763@gmail.com>
2021-10-17 11:57:10 +0800

    menambahkan column google_id di table users

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('auth/google', [App\Http\Controllers\GoogleController::class, 'redirectToGoogle'])->name('google.login');
